401b42be39168b00a94b3ec8fcfe79a0671fa662
Saltefanden <nicklas.m.larsen@gmail.com>
2026-07-26 22:39:16 +0200

    Make inline prompt

--- config.def.h
+++ config.def.h
@@ -16,6 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemePrompt] = { "#444444", "#222222" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
